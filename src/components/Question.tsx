dd3b8c2a6f775993497a1e9ab78db2b70b32fd0a
Graziele Dantas <elentary@gmail.com>
2021-06-25 16:27:30 -0300

    Week 04 done

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
+import cx from "classnames";
 
-import "../styles/question.scss"
+import "../styles/question.scss";
 
 type QuestionProps = {
   content: string;
@@ -9,20 +10,33 @@ type QuestionProps = {
     avatar: string;
   };
   children?: ReactNode; //Significa qualquer texto JSX: texto, div, basicamente QUALQUER COISA!
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 };
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({
+  content,
+  author,
+  isAnswered = false,
+  isHighlighted = false,
+  children,
+}: QuestionProps) {
   return (
-    <div className="question">
+    <div
+      className={cx( //só é possível assim pelo npm classnames
+        "question",
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+        //Normalmente usaríamos assim: classname={`question ${isAnswered ? "answered" : ""} ${isHighlighted ? "highlighted" : ""}`};
+      )}
+    >
       <p>{content}</p>
       <footer>
         <div className="user-info">
           <img src={author.avatar} alt={author.name} />
           <span>{author.name}</span>
         </div>
-        <div>
-          {children}
-        </div>
+        <div>{children}</div>
       </footer>
     </div>
   );
